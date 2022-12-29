b699f12f1f9b3a6b8e466b4b870ff4acfaf8bcf3
Joachim Lengacher <joachim.lengacher@collaboration-factory.de>
2022-12-29 22:44:00 +0100

    Revert "First draft dependency chart."
    This reverts commit e6d857585a6b661e0f489c93a26a85374080ecec.

--- src/main/webapp/app/shared/shared-libs.module.ts
+++ src/main/webapp/app/shared/shared-libs.module.ts
@@ -5,19 +5,8 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { TranslateModule } from '@ngx-translate/core';
-import { DependencyChartComponent } from './dependency-chart/dependency-chart.component';
 
 @NgModule({
-  exports: [
-    FormsModule,
-    CommonModule,
-    NgbModule,
-    InfiniteScrollModule,
-    FontAwesomeModule,
-    ReactiveFormsModule,
-    TranslateModule,
-    DependencyChartComponent,
-  ],
-  declarations: [DependencyChartComponent],
+  exports: [FormsModule, CommonModule, NgbModule, InfiniteScrollModule, FontAwesomeModule, ReactiveFormsModule, TranslateModule],
 })
 export class SharedLibsModule {}
